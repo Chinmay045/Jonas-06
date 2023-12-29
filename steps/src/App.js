c4ef29b93172cc490d1df6a586b06f15c5152138
Chinmay <chinz.sandur07@gmail.com>
2023-12-29 07:09:08 +0530

    add logic to display message

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,14 +1,21 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 
 export default function App() {
+  const step = 2;
+
   return <div className="steps">
     <div className="numbers">
-      <div>1</div>
+      <div className="active">1</div>
       <div>2</div>
       <div>3</div>
     </div>
 
-    <p className="message">Hello</p>
+    <p className="message">Step: {step}: {messages[step - 1]}Hello</p>
 
     <div className="buttons">
       <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>Previous</button>
