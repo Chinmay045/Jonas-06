197b0a66793d2967e11d3827c243b351f991a108
Chinmay <chinz.sandur07@gmail.com>
2023-12-31 22:24:13 +0530

    add another state

--- steps/src/App.js
+++ steps/src/App.js
@@ -23,7 +23,7 @@ export default function App() {
       <div className={`${step >= 3 ? "active" : ""}`}>3</div>
     </div>
 
-    <p className="message">Step: {step}: {messages[step - 1]}Hello</p>
+    <p className="message">Stepp: {step}: {messages[step - 1]}Hello</p>
 
     <div className="buttons">
       <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handlePreious}>Previous</button>
